bb0a39dd51deff56c0a20926ef22ec4fcac98c78
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-24 14:36:05 -0500

    add pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,18 @@ const currentWindEl = document.getElementById("wind-speed");
 const currentUVEl = document.getElementById("UV-index");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 console.log(searchHistory);
+
+// when search button is clicked, read city name typed by the user
+
+// using city name, get request from open weather map api
+
+// using saved city name, execute 5 day forecast request from open weather map api
+
+// display response for 5 day forecast underneath current conditions
+
+// add event listener to search button
+    // call search history
+
+// add function to render search history
+
+// create search history function and call when search button is clicked
