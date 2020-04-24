0ff7433e8e82bd58274d65a7b2b491bc31180d36
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-24 14:05:39 -0500

    add constants

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Create constants
+const cityInputEl = document.getElementById("city-input");
+const searchButtonEl = document.getElementById("search-button");
+const cityNameEl = document.getElementById("city-name");
+const currentTempEl = document.getElementById("temperature");
+const currentHumidityEl = document.getElementById("humidity");
+const currentWindEl = document.getElementById("wind-speed");
+const currentUVEl = document.getElementById("UV-index");
+var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+console.log(searchHistory);
