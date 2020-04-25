441b6100ccf0131e9e9b6d0c777c18d4e6dbe748
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-24 19:07:12 -0500

    add to getweather func

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,26 +10,50 @@ var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 console.log(searchHistory);
 
 // create a API key constant
-const APIKey = "b776531cc2c9f6ed2bb5784b21325065"
+const APIKey = "b776531cc2c9f6ed2bb5784b21325065";
 
 // when search button is clicked, read city name typed by the user
+// using city name, get request from open weather map api
 function getWeather(cityName) {
-    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid" + APIKey;
-    axios.get(queryURL)
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid" + APIKey;
+    fetch(weatherURL)
     .then(function(response) {
         console.log(response);
-    })
+
+        const currentDate = newDate(response.data.dt*1000);
+        console.log(currentDate);
+        const day = currentDate.getDate();
+        const month = currentDate.getMonth() + 1;
+        const year = currentDate.getFullYear();
+        cityNameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+        currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + "&#176F";
+        currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+        currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+    let lat = response.data.coord.lat;
+    let lon = response.data.coord.lon;
+    let UVWeatherURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+    fetch(UVWeatherURL)
+    .then(function(response) {
+        letUVIndex = document.createElement("span");
+        UVIndex.setAttribute("class", "badge badge-danger");
+        UVIndex.innerHTML = response.data[0].value;
+        currentUVEl.innerHTML = "UV Index: ";
+        currentUVEl.append(UVIndex);
+    });
 
 
 
     
+    });
+
 }
+    
 
-// using city name, get request from open weather map api
+    // using saved city name, execute 5 day forecast request from open weather map api
 
-// using saved city name, execute 5 day forecast request from open weather map api
+    // display response for 5 day forecast underneath current conditions
+    
 
-// display response for 5 day forecast underneath current conditions
 
 // add event listener to search button
     // call search history
